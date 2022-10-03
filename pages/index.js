30e92ef66d9d2f4fc9730723ec62220172e22973
Joshua Killingsworth <j05h.kwth@gmail.com>
2022-10-03 17:46:56 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">RailPod!</a>
         </h1>
+
+        <h3 className={styles.subtitle}>
+          A place for realtime shipment collaboration and notification
+        </h3>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -30,24 +34,6 @@ export default function Home() {
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
           </a>
         </div>
       </main>
